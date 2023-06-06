69ea921d6b65b00fbe77ad667e80da440018e95c
adhishtec <92138515+adhishtec@users.noreply.github.com>
2023-06-06 09:26:47 +0200

    1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import './style.css';
+import myChild from './childcompo';
 
 export default function App() {
+  const [getTextValue, setTextValue] = React.useState(0);
+
+  const [incValue, setIncValue] = React.useState(0);
+
   function changeText(val) {
-    console.log(val.value);
+    console.log(val.target.value);
+    setTextValue(squire(val.target.value));
+    console.log('Value Chanbge ❤️');
+  }
+
+  function squire(val) {
+    console.log('Value Chanbge ❤️');
+    return Math.pow(val, 2);
   }
 
   return (
     <div>
       <input onChange={(event) => changeText(event)} />
-      {}
-      <button>Ok++</button>
+      {getTextValue + '❤️'}
+      <button onClick={() => setIncValue(incValue + 1)}>Ok++</button>
+      {incValue}
     </div>
   );
 }
---
+++ src/childcompo.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+function myChild() {
+  return <div>Child Component</div>;
+}
+export default myChild;
